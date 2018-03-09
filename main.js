a3120afa3c9ae4d1553df52be49cf65871dfdeb0
Steponas Ivasovas <steponas@hotmail.com>
2018-03-09 18:57:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const {get} = require('request-promise');
+
+//paskutinis parametras nurodo kiek kartu padaryti uzklausa, nenurodzius daro begalybe kartu arba kol
+//pagauna klaida
+function getEvery(uri, ms, times = Infinity) {
+  let counter = 0;
+  let retry = 0;
+  let options = {
+    method: 'GET',
+    uri,
+    resolveWithFullResponse: true
+  };
+
+  const resolve = response => {
+    console.log(`Bitcoin price $${JSON.parse(response.body)["last_price"]}`);
+    counter++;
+    if (counter < times) {
+      setTimeout(() => { get(options).then(resolve).catch(reject) }, ms);
+    }
+  };
+
+  const reject = error => {
+    console.log(`Something went wrong, status code : ${error.statusCode}`);
+
+    if (retry < 5) {
+      console.log(`Retrying request, attempt : ${++retry}`);
+      get(options).then(resolve).catch(reject);
+    } else {
+      console.log(`Failed to receive a response after ${retry} tries... Aborting connection`);
+    }
+  };
+
+  return () => { get(options).then(resolve).catch(reject) };
+}
+
+//siuncia requesta kas 10 sekundziu 5 kartus.
+const getEvery10 = getEvery('https://api.bitfinex.com/v1/pubticker/btcusd', 10 * 1000, 5);
+getEvery10();
